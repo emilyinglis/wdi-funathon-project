98e001331b08cbf96ff3382222ff3664ac4dad24
emilyinglis <emilyameliainglis@gmail.com>
2015-12-07 11:48:38 +0000

    Basic auth setup with login and register

---
+++ solution-code-projects-users/api/config/routes.js
@@ -0,0 +1,22 @@
+var express = require('express'),
+    router  = express.Router();
+
+var usersController = require('../controllers/usersController');
+var authenticationsController = require('../controllers/authenticationsController');
+
+router.post('/login', authenticationsController.login);
+router.post('/register', authenticationsController.register);
+
+router.route('/')
+  .get(usersController.usersIndex)
+ 
+router.route('/users')
+  .get(usersController.usersIndex)
+//   .post(usersController.usersCreate)
+
+router.route('/users/:id') 
+  .get(usersController.usersShow)
+  .patch(usersController.usersUpdate)
+  .delete(usersController.usersDelete)
+
+module.exports = router;
---
+++ solution-code-projects-users/api/db/seeds.js
@@ -0,0 +1,63 @@
+var mongoose = require("mongoose");
+
+var databaseURL = 'mongodb://localhost:27017/yearbook';
+mongoose.connect(databaseURL);
+
+var User    = require("../models/user");
+
+var user1 = new User({
+  name: "Ben Layer",
+  github: "benlayer291",
+  bio: "Education tech is my ting.",
+  portfolio: "http://www.benlayer.com"
+})
+
+user1.save(function(err, user) {
+ if (err) return console.log(err);
+ console.log("User saved! ", user);
+})
+
+var user2 = new User({
+  name: "Jake Fleming",
+  github: "JakeyPedz",
+  bio: "Anecjokes are my ting.",
+  portfolio: "http://www.jakefleming.com"
+})
+
+user2.save();
+
+var user3 = new User({
+  name: "Guus Van Ooijen",
+  github: "gvop",
+  bio: "Cool trainers are my ting.",
+  portfolio: "http://www.guusvanooijen.com"
+})
+
+user3.save();
+
+var user4 = new User({
+  name: "Ollie Holden",
+  github: "odholden",
+  bio: "Skateboarding is my ting.",
+  portfolio: "http://www.ollieholden.com"
+})
+
+user4.save();
+
+var user5 = new User({
+  name: "Marika Devan",
+  github: "webdev11",
+  bio: "Films are my ting.",
+  portfolio: "http://www.marikadevan.com"
+})
+
+user5.save();
+
+var user6 = new User({
+  name: "Alastair Knowles",
+  github: "adjknowles",
+  bio: "Music is my ting.",
+  portfolio: "http://www.alastairknowles.com"
+})
+
+user6.save();
