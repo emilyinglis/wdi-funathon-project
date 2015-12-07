1b0d4820157c0b98d1dfc9828b5726cc5f0987e9
adjknowles <alastair_knowles@yahoo.co.uk>
2015-12-07 20:37:18 +0000

    Completed a styling overhaul

---
+++ solution-code-projects-users/front-end/js/calendar.js
@@ -0,0 +1,119 @@
+$('#open1').on('click', function (e) {
+  e.preventDefault();
+  $("#open1").css("background-image", 'url(./assets/images/1.png)');
+});
+
+$('#open2').on('click', function (e) {
+  e.preventDefault();
+  $("#open2").css("background-image", 'url(./assets/images/2.png)');
+});
+
+$('#open3').on('click', function (e) {
+  e.preventDefault();
+  $("#open3").css("background-image", 'url(./assets/images/3.png)');
+});
+
+$('#open4').on('click', function (e) {
+  e.preventDefault();
+  $("#open4").css("background-image", 'url(./assets/images/4.png)');
+});
+
+$('#open5').on('click', function (e) {
+  e.preventDefault();
+  $("#open5").css("background-image", 'url(./assets/images/5.png)');
+});
+
+$('#open6').on('click', function (e) {
+  e.preventDefault();
+  $("#open6").css("background-image", 'url(./assets/images/6.png)');
+});
+
+$('#open7').on('click', function (e) {
+  e.preventDefault();
+  $("#open7").css("background-image", 'url(./assets/images/7.png)');
+});
+
+$('#open8').on('click', function (e) {
+  e.preventDefault();
+  $("#open8").css("background-image", 'url(./assets/images/8.png)');
+});
+
+$('#open9').on('click', function (e) {
+  e.preventDefault();
+  $("#open9").css("background-image", 'url(./assets/images/9.png)');
+});
+
+$('#open10').on('click', function (e) {
+  e.preventDefault();
+  $("#open10").css("background-image", 'url(./assets/images/10.png)');
+});
+
+$('#open11').on('click', function (e) {
+  e.preventDefault();
+  $("#open11").css("background-image", 'url(./assets/images/11.png)');
+});
+
+$('#open12').on('click', function (e) {
+  e.preventDefault();
+  $("#open12").css("background-image", 'url(./assets/images/12.png)');
+});
+
+$('#open13').on('click', function (e) {
+  e.preventDefault();
+  $("#open13").css("background-image", 'url(./assets/images/13.png)');
+});
+
+$('#open14').on('click', function (e) {
+  e.preventDefault();
+  $("#open14").css("background-image", 'url(./assets/images/14.png)');
+});
+
+$('#open15').on('click', function (e) {
+  e.preventDefault();
+  $("#open15").css("background-image", 'url(./assets/images/15.png)');
+});
+
+$('#open16').on('click', function (e) {
+  e.preventDefault();
+  $("#open16").css("background-image", 'url(./assets/images/16.png)');
+});
+
+$('#open17').on('click', function (e) {
+  e.preventDefault();
+  $("#open17").css("background-image", 'url(./assets/images/17.png)');
+});
+
+$('#open18').on('click', function (e) {
+  e.preventDefault();
+  $("#open18").css("background-image", 'url(./assets/images/18.png)');
+});
+
+$('#open19').on('click', function (e) {
+  e.preventDefault();
+  $("#open19").css("background-image", 'url(./assets/images/19.png)');
+});
+
+$('#open20').on('click', function (e) {
+  e.preventDefault();
+  $("#open20").css("background-image", 'url(./assets/images/20.png)');
+});
+
+$('#open21').on('click', function (e) {
+  e.preventDefault();
+  $("#open21").css("background-image", 'url(./assets/images/21.png)');
+});
+
+$('#open22').on('click', function (e) {
+  e.preventDefault();
+  $("#open22").css("background-image", 'url(./assets/images/22.png)');
+});
+
+$('#open23').on('click', function (e) {
+  e.preventDefault();
+  $("#open23").css("background-image", 'url(./assets/images/23.png)');
+});
+
+$('#open24').on('click', function (e) {
+  e.preventDefault();
+  $("#open24").css("background-image", 'url(./assets/images/24.png)');
+});
